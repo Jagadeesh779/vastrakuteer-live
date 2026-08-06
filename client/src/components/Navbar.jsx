8b3ef4a597cdedb467656083625a5ec8da2c32c2
Jagadeesh779 <vastrakuteer9@gmail.com>
2026-08-06 14:45:54 +0530

    style: set marginLeft 80px and 84px on brand text spans in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -70,8 +70,8 @@ const Navbar = () => {
                                 <img src={logo} alt="Vastra Kuteer" style={{ height: '74px', width: 'auto', objectFit: 'contain' }} />
                                 {/* Brand Name Container (Move ONLY the name using marginLeft or transform) */}
                                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', lineHeight: 0.95, marginLeft: '0px', transform: 'translateX(0px)' }}>
-                                    <span style={{ fontFamily: "'Great Vibes', cursive", fontSize: '2.8rem', fontWeight: 500, color: '#1a5c3a', lineHeight: 0.95, marginLeft: '40px' }}>Vastra</span>
-                                    <span style={{ fontFamily: 'Georgia, serif', fontSize: '0.95rem', fontWeight: 700, letterSpacing: '0.35em', color: '#1a1a1a', textTransform: 'uppercase', marginTop: '-2px', marginLeft: '40px' }}>KUTEER</span>
+                                    <span style={{ fontFamily: "'Great Vibes', cursive", fontSize: '2.8rem', fontWeight: 500, color: '#1a5c3a', lineHeight: 0.95, marginLeft: '80px' }}>Vastra</span>
+                                    <span style={{ fontFamily: 'Georgia, serif', fontSize: '0.95rem', fontWeight: 700, letterSpacing: '0.35em', color: '#1a1a1a', textTransform: 'uppercase', marginTop: '-2px', marginLeft: '84px' }}>KUTEER</span>
                                 </div>
                             </Link>
 
